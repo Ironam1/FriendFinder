39d84b8d26cfb8e6ea4e517d2c37bdfe9784e3b6
Ironam1 <ironam1@icloud.com>
2019-07-22 18:05:15 -0400

    linked in the tables to diplay data on page. need to make questions, finish logic for comparing answers to questions.

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+const friends = [
+  {
+    name: "Doug",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fdoug%2Fimages%2Fa%2Fa5%2FDoug_Funnie2.png%2Frevision%2Flatest%3Fcb%3D20180531191407&imgrefurl=https%3A%2F%2Fdoug.fandom.com%2Fwiki%2FDoug_Funnie&docid=ikWAUwB9k_CYmM&tbnid=j5V_HSIvi6FYBM%3A&vet=10ahUKEwjwmu7_p8jjAhWkmeAKHYGaAxkQMwh8KAEwAQ..i&w=428&h=604&bih=712&biw=972&q=doug&ved=0ahUKEwjwmu7_p8jjAhWkmeAKHYGaAxkQMwh8KAEwAQ&iact=mrc&uact=8",
+    scores: ["2", "3", "2", "5", "4", "1", "1", "3", "2", "1"]
+  }
+];
+module.exports = friends;
--- server.js
+++ server.js
@@ -6,8 +6,20 @@ const PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const friends = require("./app/data/friends");
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+app.get("/api/friends", function(req, res) {
+  res.json(friends);
+});
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
